080c3bfacf09e832a30bca5f5be3e819ce2bee69
Eric Selover <eric.selover@gmail.com>
2015-11-16 00:08:43 -0500

    Started coding the Home Page
    Coded the HTML, and CSS of the website

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Eric Selover</title>
+    <link href="style/main.css" rel="stylesheet">
+    <script src="js/jquery-2.1.4.min.js"></script>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php"><img id="logo" src="imgs/logo.png"></a>
+            <ul>
+                <li><a id = "home" href="index.php">Home</a></li>
+                <li><a id = "port" href="#">Portfolio</a></li>
+                <li><a id = "about" href="#">About</a></li>
+                <li><a id = "contact" href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <div id="container">
+        
+        <a id="proj1" href="#">
+            <img id = "img1" src="imgs/triviaGame.png">
+            <div>
+                <h3>GRPM Trivia Game</h3>
+                <p>Platform: Apple iPad</p>
+                <p>ActionScript 3.0, XML</p>
+            </div>
+        </a>
+        
+        <a id="proj2" href="#">
+            <img id = "img2" src="imgs/flowField.png">
+            <div>
+                <h3>Flow Field</h3>
+                <p>Processing</p>
+            </div>
+        </a>
+        
+        <a id="proj3" href="#">
+            <img id = "img3" src="imgs/emergence.png">
+            <div>
+                <h3>Emergence</h3>
+                <p>Processing 3D</p>
+            </div>
+        </a>
+        
+        <a id="proj4" href="#">
+            <img id = "img4" src="imgs/bullyGame.png">
+            <div>
+                <h3>Breaking Point</h3>
+                <p>Platform: PC</p>
+                <p>ActionScript 3.0</p>
+            </div>
+        </a>
+        
+        <a id="proj5" href="#">
+            <img id = "img5" src="imgs/musicVis.png">
+            <div>
+                <h3>Junior Project Music Visualizer</h3>
+                <p>Client: GRPM Planetarium</p>
+                <p>Processing, OpenGL Shaders</p>
+            </div>
+        </a>
+        
+        <a id="proj6" href="#">
+            <img id = "img6" src="imgs/MusicVis2.png">
+            <div>
+                <h3>Music Visualizer</h3>
+                <p>Processing 3D, OpenGL Shaders</p>
+            </div>
+        </a>
+
+        <div class="clear"></div>
+    </div>
+    
+    <div id="footer">
+        <span id="footerContent">&copy2015 Eric Selover</span>
+    </div>
+    
+</body>
+</html>
